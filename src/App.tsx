bd178004341245e42ae9c2984bd79a644067b060
YASH KHANWILKAR <ykhanwilkar@gmail.com>
2025-03-03 00:12:54 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+      <div>
+        React app with Shadcnui
+        <Button>Click me</Button>
+      </div>
+  )
+}
+
+export default App
